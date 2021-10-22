3ef5498a594b81303a50ead7d6ac9f7317a6b94f
Iliyan Vlahov <iliyan.vlahov@gmail.com>
2021-10-22 14:44:51 +0300

    fix the nasty bug trolling me for 2 hours

--- server/api/middlewares/email.constraint.handler.js
+++ server/api/middlewares/email.constraint.handler.js
@@ -2,5 +2,5 @@ import { DUPLICATE_EMAIL } from '../../common/constants';
 
 // eslint-disable-next-line no-unused-vars, no-shadow
 export default function emailConstaintErrorHandler(err, req, res, next) {
-  err && err.code === '23505' ? next(DUPLICATE_EMAIL) : next();
+  err && err.code === '23505' ? next(DUPLICATE_EMAIL) : next(err);
 }
